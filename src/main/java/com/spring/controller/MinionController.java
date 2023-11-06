58272669df6130c624476929b0c2b028af128064
MadForLife <madforlifee@gmail.com>
2023-11-06 17:40:51 +0200

    Created "createMinion.html" - Form
    Included new methods in the MinionController: showCreateMinionForm, createMinion

--- src/main/java/com/spring/controller/MinionController.java
+++ src/main/java/com/spring/controller/MinionController.java
@@ -3,8 +3,11 @@ package com.spring.controller;
 import com.spring.model.Minion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.service.MinionService;
 
@@ -25,6 +28,18 @@ public class MinionController {
         List<Minion> minionList = minionService.findAll();
         model.addAttribute("minions", minionList);
         return "minions";
+    }
+
+    //Shows the create minion form in the browser
+    @GetMapping("/create")
+    public String showCreateMinionForm() {
+        return "createMinion";
+    }
+
+    @PostMapping("/insert")
+    public String createMinion(@ModelAttribute Minion minion) {
+        minionService.save(minion);
+        return "redirect:/minions/all";
     }
 
 }
